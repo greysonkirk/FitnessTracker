f1198e7c204fb83a9da16eef4bb458210ade1ec3
Greyson Kirk <greysonkirk@yahoo.com>
2020-12-20 11:18:26 -0600

    added models

---
+++ models/activity.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [{
+        type: {
+            type: String,
+            trim: true,
+        },
+        name: {
+            type: String,
+            trim: true,
+        },
+        duration: Number,
+        weight: {
+            type: Number,
+            default: 0
+        },
+        reps: {
+            type: Number,
+            default: 0
+        },
+        sets: {
+            type: Number,
+            default: 0
+        },
+        distance: {
+            type: Number,
+            default: 0
+        }
+    }]
+
+
+})
+
+workoutSchema.virtual("totalDuration").get(function() {
+    for (let i = 0; i < this.exercises.length; i++) {
+        const element = this.exercises[i];
+        totalDuration = SUM(this.exercises.duration)
+    }
+});
+
+
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
